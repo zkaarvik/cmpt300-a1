cb068c4c1f6de73750d5e4a217fe94e45b7d65e2
Zachary Kaarvik <zkaarvik@gmail.com>
2015-01-08 23:45:17 -0800

    Create part 1, hello world program, add makefile, and add .gitignore for executable

---
+++ part1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    getchar();
+    return 0;
+}
+
+
+// /proc/cpuinfo - cpu info
+// /proc/version - kernel info
+// /proc/meminfo - memory info
+// /proc/uptime - uptime

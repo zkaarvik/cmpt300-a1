72999ae0ad2d0e606a0cc32797e4f7d33b30395f
Zachary Kaarvik <zkaarvik@gmail.com>
2015-01-10 23:56:13 -0800

    Basic parsing of /proc/cpuinfo to find model name

--- part1/main.c
+++ part1/main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 256
 
 int main()
 {
-    printf("Hello World!\n");
-    getchar();
+    char input_line[MAX_LINE_LENGTH];
+    FILE *fp_cpuinfo;
+
+    // Read cpuinfo file and obtain processor type
+    fp_cpuinfo = fopen("/proc/cpuinfo", "r");
+    while (fgets(input_line, MAX_LINE_LENGTH, fp_cpuinfo))
+    {
+        //printf("%s", input_line);
+        if (strstr(input_line, "model name") != NULL)
+        {
+            printf("%s", input_line);
+        }
+    }
+    fclose(fp_cpuinfo);
+
+
+    //printf("Hello World!\n%s", input_line);
+    //getchar();
     return 0;
 }
 

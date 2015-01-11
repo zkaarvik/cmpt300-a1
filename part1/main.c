8c1b5b809a0b908d36a1c8b1d306d582f67ec035
Zachary Kaarvik <zkaarvik@gmail.com>
2015-01-11 15:54:19 -0800

    Part 1 functionality complete, all req'd info is being retreived

--- part1/main.c
+++ part1/main.c
@@ -3,14 +3,20 @@
 
 #define MAX_LINE_LENGTH 256
 #define ID_CPUINFO "model name\t: "
+#define ID_MEMINFO "MemTotal:"
 
 int main()
 {
     char str_input_line[MAX_LINE_LENGTH];
     size_t index;
     FILE *fp_cpuinfo;
+    FILE *fp_kernelversion;
+    FILE *fp_meminfo;
+    FILE *fp_uptime;
 
-    // Obtain processor type
+    /*
+     *  Obtain processor type
+     */
     fp_cpuinfo = fopen("/proc/cpuinfo", "r");
     while (fgets(str_input_line, MAX_LINE_LENGTH, fp_cpuinfo))
     {
@@ -23,12 +29,42 @@ int main()
         }
     }
     fclose(fp_cpuinfo);
-    
+
+    /*
+     *  Obtain kernel version
+     */
+    fp_kernelversion = fopen("/proc/sys/kernel/osrelease", "r");
+    //The only line in the file should be the kernel version
+    fgets(str_input_line, MAX_LINE_LENGTH, fp_kernelversion);
+    printf("Kernel version: %s", str_input_line);
+    fclose(fp_kernelversion);
+
+    /*
+     *  Obtain total memory
+     */
+    fp_meminfo = fopen("/proc/meminfo", "r");
+    while (fgets(str_input_line, MAX_LINE_LENGTH, fp_meminfo))
+    {
+        //Check for line beginning with "MemTotal:", it is followed by an unknown number of spaces before the memory amount
+        if (strstr(str_input_line, ID_MEMINFO) != NULL)
+        {
+            index = strlen(ID_MEMINFO);
+            while(str_input_line[index] == ' ') index++;
+            printf("Total memory: %s", str_input_line + index);
+            break;
+        }
+    }
+    fclose(fp_meminfo);
+
+    /*
+     *  Obtain uptime
+     */
+    fp_uptime = fopen("/proc/uptime", "r");
+    //Use scanf to get the first characters in the file up to the first whitespace character, which is the uptime in seconds
+    //fgets(str_input_line, MAX_LINE_LENGTH, fp_uptime);
+    fscanf(fp_uptime, "%s", str_input_line);
+    printf("System uptime: %s seconds\n", str_input_line);
+    fclose(fp_uptime);
+
     return 0;
 }
-
-
-// /proc/cpuinfo - cpu info
-// /proc/version - kernel info
-// /proc/meminfo - memory info
-// /proc/uptime - uptime

9c25a4320537004a1c7ff38c28f72eebadbec562
Zachary Kaarvik <zkaarvik@gmail.com>
2015-01-18 16:04:52 -0800

    Execution wokring, with bugs

--- part2/main.c
+++ part2/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <libgen.h>
 
 #define MAX_INPUT_LENGTH 4096
 #define DELIMS " \t\n\r"
@@ -15,6 +16,7 @@ int main()
     char **cur_argv;
     int cur_argc;
     int i;
+    pid_t pid;
 
     //main loop
     while (1)
@@ -31,8 +33,11 @@ int main()
         }
         else continue;
 
-        //Then each subsequent token is an argument (until we see | or &)
+        //First argument must be the filename (req'd for execvp)
+        //each subsequent token is an argument (until we see | or &)
         cur_argv = malloc(sizeof(char *));
+        cur_argc++;
+        cur_argv[cur_argc-1] = basename(cur_prog);
         while (cur_token = strtok_r(NULL, DELIMS, &saveptr))
         {
             cur_argc++;
@@ -49,18 +54,32 @@ int main()
 
 
         //*******DEBUG
-        printf("program name: %s \n", cur_prog);
-        for (i = 0; i < cur_argc; i++)
-        {
-            printf("argv array[%i]: %s \n", i, cur_argv[i]);
-        }
+        // printf("program name: %s \n", cur_prog);
+        // for (i = 0; i < cur_argc; i++)
+        // {
+        //     printf("argv array[%i]: %s \n", i, cur_argv[i]);
+        // }
         //END *******DEBUG
 
 
 
 
         //We have the program/file to execute and a list of arguments.. now we can fork and execute
-
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("Error: Fork failure");
+            continue;
+        }
+        else if (pid == 0) //Running in the child process, run command
+        {
+            execvp(cur_prog, cur_argv);
+        }
+        else //Running in the parent process, wait for child to complete
+        {
+            wait(NULL);
+            printf("Child finished\n");
+        }
 
 
 
